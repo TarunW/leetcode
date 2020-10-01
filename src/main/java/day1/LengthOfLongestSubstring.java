dc9799b32f2e033adb54ae2a9dc1ed44893926a0
Tarun Walia <Tarun.Walia@walmart.com>
2020-10-02 02:12:05 +0530

    WIp

--- src/main/java/day1/LengthOfLongestSubstring.java
+++ src/main/java/day1/LengthOfLongestSubstring.java
@@ -6,20 +6,28 @@ import java.util.Set;
 public class LengthOfLongestSubstring {
 
     public static int lengthOfLongestSubstring(String s) {
-        int maxLen = s.length() <= 1 ? s.length() : 1;
+        int n = s.length();
         Set<Character> set = new HashSet<>();
-        for (char ch : s.toCharArray()) {
-            if (!set.add(ch)) {
-                maxLen = set.size() > maxLen ? set.size() : maxLen;
-                set.clear();
-                set.add(ch);
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
             }
         }
-        return maxLen;
+        return ans;
     }
 
+
+
     public static void main(String[] args) {
-        System.out.println("lengthOfLongestSubstring(\"abcabcbb\") = " + lengthOfLongestSubstring("q"));
+        System.out.println("lengthOfLongestSubstring(\"abcabcbb\") = " + lengthOfLongestSubstring("bbbbb"));
     }
+
+
 }
 
